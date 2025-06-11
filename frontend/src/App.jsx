b178425f39d4b6ad074678bef5511fd58d74f991
true1853 <true@T-book.localdomain>
2025-06-11 14:25:15 +0300

    fix: name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,7 +45,7 @@ const AdminLayout = ({ setToken }) => {
       <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
         <div className="flex flex-col flex-grow pt-5 bg-gray-800 overflow-y-auto">
           <div className="flex items-center flex-shrink-0 px-4">
-             <h1 className="text-white text-2xl font-semibold">Briefly</h1>
+             <h1 className="text-white text-2xl font-semibold">Брифли</h1>
           </div>
           <div className="mt-5 flex-1 flex flex-col">
             <nav className="flex-1 px-2 pb-4 space-y-1">
